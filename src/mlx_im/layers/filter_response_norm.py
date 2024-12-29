2de4bd31cd9585de3cdbdae3854937808dddb01f
Louis Lac <lac.louis5@gmail.com>
2024-12-29 23:59:44 +0100

    [DEV] Filter response norm

--- src/mlx_im/layers/filter_response_norm.py
+++ src/mlx_im/layers/filter_response_norm.py
@@ -1,86 +1,71 @@
-"""Filter Response Norm in PyTorch
-
-Based on `Filter Response Normalization Layer` - https://arxiv.org/abs/1911.09737
-
-Hacked together by / Copyright 2021 Ross Wightman
-"""
-
-import torch
-import torch.nn as nn
+import mlx.core as mx
+from mlx import nn
 
 from .create_act import create_act_layer
 from .trace_utils import _assert
 
 
-def inv_instance_rms(x, eps: float = 1e-5):
-    rms = x.square().float().mean(dim=(2, 3), keepdim=True).add(eps).rsqrt().to(x.dtype)
-    return rms.expand(x.shape)
+def inv_instance_rms(x: mx.array, eps: float = 1e-5) -> mx.array:
+    rms = (x.square().mean(axis=(1, 2), keepdims=True) + eps).rsqrt()
+    return mx.broadcast_to(rms, shape=x.shape)
 
 
 class FilterResponseNormTlu2d(nn.Module):
-    def __init__(self, num_features, apply_act=True, eps=1e-5, rms=True, **_):
-        super(FilterResponseNormTlu2d, self).__init__()
-        self.apply_act = apply_act  # apply activation (non-linearity)
+    def __init__(
+        self,
+        num_features: int,
+        apply_act: bool = True,
+        eps: float = 1e-5,
+        rms: bool = True,
+        **_,
+    ):
+        super().__init__()
+        self.apply_act = apply_act
         self.rms = rms
         self.eps = eps
-        self.weight = nn.Parameter(torch.ones(num_features))
-        self.bias = nn.Parameter(torch.zeros(num_features))
-        self.tau = nn.Parameter(torch.zeros(num_features)) if apply_act else None
-        self.reset_parameters()
 
-    def reset_parameters(self):
-        nn.init.ones_(self.weight)
-        nn.init.zeros_(self.bias)
-        if self.tau is not None:
-            nn.init.zeros_(self.tau)
+        self.weight = mx.ones(num_features)
+        self.bias = mx.zeros(num_features)
+        self.tau = mx.zeros(num_features) if apply_act else None
 
-    def forward(self, x):
-        _assert(x.dim() == 4, "expected 4D input")
-        x_dtype = x.dtype
-        v_shape = (1, -1, 1, 1)
+    def __call__(self, x: mx.array) -> mx.array:
+        _assert(x.ndim == 4, "expected 4D input")
+        v_shape = (1, 1, 1, -1)
+
         x = x * inv_instance_rms(x, self.eps)
-        x = x * self.weight.view(v_shape).to(dtype=x_dtype) + self.bias.view(
-            v_shape
-        ).to(dtype=x_dtype)
-        return (
-            torch.maximum(x, self.tau.reshape(v_shape).to(dtype=x_dtype))
-            if self.tau is not None
-            else x
-        )
+        x = x * self.weight.reshape(v_shape) + self.bias.reshape(v_shape)
+
+        return mx.maximum(x, self.tau.reshape(v_shape)) if self.tau is not None else x
 
 
 class FilterResponseNormAct2d(nn.Module):
     def __init__(
         self,
-        num_features,
-        apply_act=True,
-        act_layer=nn.ReLU,
-        inplace=None,
-        rms=True,
-        eps=1e-5,
+        num_features: int,
+        apply_act: bool = True,
+        act_layer: str | nn.Module | None = nn.ReLU,
+        inplace: bool = None,
+        rms: bool = True,
+        eps: float = 1e-5,
         **_,
     ):
-        super(FilterResponseNormAct2d, self).__init__()
+        super().__init__()
         if act_layer is not None and apply_act:
             self.act = create_act_layer(act_layer, inplace=inplace)
         else:
             self.act = nn.Identity()
+
         self.rms = rms
         self.eps = eps
-        self.weight = nn.Parameter(torch.ones(num_features))
-        self.bias = nn.Parameter(torch.zeros(num_features))
-        self.reset_parameters()
 
-    def reset_parameters(self):
-        nn.init.ones_(self.weight)
-        nn.init.zeros_(self.bias)
+        self.weight = mx.ones(num_features)
+        self.bias = mx.zeros(num_features)
 
-    def forward(self, x):
-        _assert(x.dim() == 4, "expected 4D input")
-        x_dtype = x.dtype
-        v_shape = (1, -1, 1, 1)
+    def __call__(self, x: mx.array) -> mx.array:
+        _assert(x.ndim == 4, "expected 4D input")
+        v_shape = (1, 1, 1, -1)
+
         x = x * inv_instance_rms(x, self.eps)
-        x = x * self.weight.view(v_shape).to(dtype=x_dtype) + self.bias.view(
-            v_shape
-        ).to(dtype=x_dtype)
+        x = x * self.weight.reshape(v_shape) + self.bias.reshape(v_shape)
+
         return self.act(x)
---
+++ tests/test_response_norm.py
@@ -0,0 +1,77 @@
+import mlx.core as mx
+import numpy as np
+import pytest
+import timm.layers.filter_response_norm as timm_m
+
+import mlx_im.layers.filter_response_norm as mlx_m
+
+from . import utils as U
+
+
+def test_inv_instance_rms():
+    x_mlx = U.sample_mlx_array_2d(shape=(1, 512, 768, 32))
+    x_torch = U.mlx_to_torch_2d(x_mlx)
+
+    out_mlx = mlx_m.inv_instance_rms(x_mlx)
+    out_timm = timm_m.inv_instance_rms(x_torch)
+
+    mx.eval(out_mlx)
+
+    out_mlx = U.mlx_to_numpy_2d(out_mlx)
+    out_timm = U.torch_to_numpy_2d(out_timm)
+
+    assert np.allclose(
+        out_mlx, out_timm, atol=1.0e-7
+    ), f"{np.max(np.abs(out_mlx - out_timm)).item()}"
+
+
+@pytest.mark.parametrize("apply_act", [True, False])
+@pytest.mark.parametrize("rms", [True, False])
+def test_filter_resp_norm_tlu_2d(apply_act, rms):
+    x_mlx = U.sample_mlx_array_2d(shape=(1, 512, 768, 64))
+    x_torch = U.mlx_to_torch_2d(x_mlx)
+
+    mod_mlx = mlx_m.FilterResponseNormTlu2d(
+        num_features=64, apply_act=apply_act, rms=rms
+    )
+    mod_timm = timm_m.FilterResponseNormTlu2d(
+        num_features=64, apply_act=apply_act, rms=rms
+    )
+
+    out_mlx = mod_mlx(x_mlx)
+    out_timm = mod_timm(x_torch)
+
+    mx.eval(out_mlx)
+
+    out_mlx = U.mlx_to_numpy_2d(out_mlx)
+    out_timm = U.torch_to_numpy_2d(out_timm)
+
+    assert np.allclose(
+        out_mlx, out_timm, atol=1.0e-7
+    ), f"{np.max(np.abs(out_mlx - out_timm)).item()}"
+
+
+@pytest.mark.parametrize("apply_act", [True, False])
+@pytest.mark.parametrize("rms", [True, False])
+def test_filter_resp_norm_act_2d(apply_act, rms):
+    x_mlx = U.sample_mlx_array_2d(shape=(1, 512, 768, 64))
+    x_torch = U.mlx_to_torch_2d(x_mlx)
+
+    mod_mlx = mlx_m.FilterResponseNormAct2d(
+        num_features=64, apply_act=apply_act, rms=rms
+    )
+    mod_timm = timm_m.FilterResponseNormAct2d(
+        num_features=64, apply_act=apply_act, rms=rms
+    )
+
+    out_mlx = mod_mlx(x_mlx)
+    out_timm = mod_timm(x_torch)
+
+    mx.eval(out_mlx)
+
+    out_mlx = U.mlx_to_numpy_2d(out_mlx)
+    out_timm = U.torch_to_numpy_2d(out_timm)
+
+    assert np.allclose(
+        out_mlx, out_timm, atol=1.0e-7
+    ), f"{np.max(np.abs(out_mlx - out_timm)).item()}"
