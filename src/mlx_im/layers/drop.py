f48f5ab554dbeec690a5b3e7ff3bf4cad04eae77
Louis Lac <lac.louis5@gmail.com>
2024-12-31 19:17:06 +0100

    [DEV] Drop block and drop path

--- src/mlx_im/layers/drop.py
+++ src/mlx_im/layers/drop.py
@@ -1,46 +1,23 @@
-"""DropBlock, DropPath
-
-PyTorch implementations of DropBlock and DropPath (Stochastic Depth) regularization layers.
-
-Papers:
-DropBlock: A regularization method for convolutional networks (https://arxiv.org/abs/1810.12890)
-
-Deep Networks with Stochastic Depth (https://arxiv.org/abs/1603.09382)
-
-Code:
-DropBlock impl inspired by two Tensorflow impl that I liked:
- - https://github.com/tensorflow/tpu/blob/master/models/official/resnet/resnet_model.py#L74
- - https://github.com/clovaai/assembled-cnn/blob/master/nets/blocks.py
-
-Hacked together by / Copyright 2020 Ross Wightman
-"""
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+import mlx.core as mx
+from mlx import nn
 
 from .grid import ndgrid
 
 
 def drop_block_2d(
-    x,
+    x: mx.array,
     drop_prob: float = 0.1,
     block_size: int = 7,
     gamma_scale: float = 1.0,
     with_noise: bool = False,
     inplace: bool = False,
     batchwise: bool = False,
-):
-    """DropBlock. See https://arxiv.org/pdf/1810.12890.pdf
-
-    DropBlock with an experimental gaussian noise option. This layer has been tested on a few training
-    runs with success, but needs further validation and possibly optimization for lower runtime impact.
-    """
-    B, C, H, W = x.shape
-    total_size = W * H
+) -> mx.array:
+    _, H, W, C = x.shape
+    total_size: int = W * H
     clipped_block_size = min(block_size, min(W, H))
-    # seed_drop_rate, the gamma parameter
-    gamma = (
+
+    gamma: float = (
         gamma_scale
         * drop_prob
         * total_size
@@ -48,66 +25,54 @@ def drop_block_2d(
         / ((W - block_size + 1) * (H - block_size + 1))
     )
 
-    # Forces the block to be inside the feature map.
-    w_i, h_i = ndgrid(
-        torch.arange(W, device=x.device), torch.arange(H, device=x.device)
-    )
+    w_i, h_i = ndgrid(mx.arange(W), mx.arange(H))
     valid_block = (
         (w_i >= clipped_block_size // 2) & (w_i < W - (clipped_block_size - 1) // 2)
     ) & ((h_i >= clipped_block_size // 2) & (h_i < H - (clipped_block_size - 1) // 2))
-    valid_block = torch.reshape(valid_block, (1, 1, H, W)).to(dtype=x.dtype)
+
+    valid_block = mx.reshape(valid_block, (1, H, W, 1))
 
     if batchwise:
-        # one mask for whole batch, quite a bit faster
-        uniform_noise = torch.rand((1, C, H, W), dtype=x.dtype, device=x.device)
+        uniform_noise = mx.random.uniform(shape=(1, H, W, C), dtype=x.dtype)
     else:
-        uniform_noise = torch.rand_like(x)
-    block_mask = ((2 - gamma - valid_block + uniform_noise) >= 1).to(dtype=x.dtype)
-    block_mask = -F.max_pool2d(
-        -block_mask,
-        kernel_size=clipped_block_size,  # block_size,
+        uniform_noise = mx.random.uniform(shape=x.shape, dtype=x.dtype)
+
+    block_mask = ((2 - gamma - valid_block + uniform_noise) >= 1).astype(dtype=x.dtype)
+    block_mask: mx.array = -nn.MaxPool2d(
+        kernel_size=clipped_block_size,
         stride=1,
         padding=clipped_block_size // 2,
-    )
+    )(-block_mask)
 
     if with_noise:
         normal_noise = (
-            torch.randn((1, C, H, W), dtype=x.dtype, device=x.device)
+            mx.random.normal(shape=(1, H, W, C), dtype=x.dtype)
             if batchwise
-            else torch.randn_like(x)
+            else mx.random.normal(shape=x.shape, dtype=x.dtype)
         )
-        if inplace:
-            x.mul_(block_mask).add_(normal_noise * (1 - block_mask))
-        else:
-            x = x * block_mask + normal_noise * (1 - block_mask)
+        x = x * block_mask + normal_noise * (1 - block_mask)
     else:
         normalize_scale = (
-            block_mask.numel() / block_mask.to(dtype=torch.float32).sum().add(1e-7)
-        ).to(x.dtype)
-        if inplace:
-            x.mul_(block_mask * normalize_scale)
-        else:
-            x = x * block_mask * normalize_scale
+            block_mask.size / (block_mask.astype(dtype=mx.float32).sum() + 1e-7)
+        ).astype(x.dtype)
+        x = x * block_mask * normalize_scale
+
     return x
 
 
 def drop_block_fast_2d(
-    x: torch.Tensor,
+    x: mx.array,
     drop_prob: float = 0.1,
     block_size: int = 7,
     gamma_scale: float = 1.0,
     with_noise: bool = False,
     inplace: bool = False,
-):
-    """DropBlock. See https://arxiv.org/pdf/1810.12890.pdf
-
-    DropBlock with an experimental gaussian noise option. Simplied from above without concern for valid
-    block mask at edges.
-    """
-    B, C, H, W = x.shape
-    total_size = W * H
+) -> mx.array:
+    _, H, W, _ = x.shape
+    total_size: int = W * H
     clipped_block_size = min(block_size, min(W, H))
-    gamma = (
+
+    gamma: float = (
         gamma_scale
         * drop_prob
         * total_size
@@ -115,35 +80,26 @@ def drop_block_fast_2d(
         / ((W - block_size + 1) * (H - block_size + 1))
     )
 
-    block_mask = torch.empty_like(x).bernoulli_(gamma)
-    block_mask = F.max_pool2d(
-        block_mask.to(x.dtype),
+    block_mask = mx.random.bernoulli(p=gamma, shape=x.shape).astype(dtype=x.dtype)
+    block_mask: mx.array = nn.MaxPool2d(
         kernel_size=clipped_block_size,
         stride=1,
         padding=clipped_block_size // 2,
-    )
+    )(block_mask)
 
     if with_noise:
-        normal_noise = torch.empty_like(x).normal_()
-        if inplace:
-            x.mul_(1.0 - block_mask).add_(normal_noise * block_mask)
-        else:
-            x = x * (1.0 - block_mask) + normal_noise * block_mask
+        normal_noise = mx.random.normal(shape=x.shape, dtype=x.dtype)
+        x = x * (1.0 - block_mask) + normal_noise * block_mask
     else:
         block_mask = 1 - block_mask
         normalize_scale = (
-            block_mask.numel() / block_mask.to(dtype=torch.float32).sum().add(1e-6)
-        ).to(dtype=x.dtype)
-        if inplace:
-            x.mul_(block_mask * normalize_scale)
-        else:
-            x = x * block_mask * normalize_scale
+            block_mask.size / (block_mask.astype(dtype=mx.float32).sum() + 1e-6)
+        ).astype(dtype=x.dtype)
+        x = x * block_mask * normalize_scale
     return x
 
 
 class DropBlock2d(nn.Module):
-    """DropBlock. See https://arxiv.org/pdf/1810.12890.pdf"""
-
     def __init__(
         self,
         drop_prob: float = 0.1,
@@ -154,18 +110,19 @@ class DropBlock2d(nn.Module):
         batchwise: bool = False,
         fast: bool = True,
     ):
-        super(DropBlock2d, self).__init__()
+        super().__init__()
         self.drop_prob = drop_prob
         self.gamma_scale = gamma_scale
         self.block_size = block_size
         self.with_noise = with_noise
         self.inplace = inplace
         self.batchwise = batchwise
-        self.fast = fast  # FIXME finish comparisons of fast vs not
+        self.fast = fast
 
-    def forward(self, x):
+    def __call__(self, x: mx.array) -> mx.array:
         if not self.training or not self.drop_prob:
             return x
+
         if self.fast:
             return drop_block_fast_2d(
                 x,
@@ -188,38 +145,31 @@ class DropBlock2d(nn.Module):
 
 
 def drop_path(
-    x, drop_prob: float = 0.0, training: bool = False, scale_by_keep: bool = True
-):
-    """Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks).
-
-    This is the same as the DropConnect impl I created for EfficientNet, etc networks, however,
-    the original name is misleading as 'Drop Connect' is a different form of dropout in a separate paper...
-    See discussion: https://github.com/tensorflow/tpu/issues/494#issuecomment-532968956 ... I've opted for
-    changing the layer and argument names to 'drop path' rather than mix DropConnect as a layer name and use
-    'survival rate' as the argument.
-
-    """
+    x: mx.array,
+    drop_prob: float = 0.0,
+    training: bool = False,
+    scale_by_keep: bool = True,
+) -> mx.array:
     if drop_prob == 0.0 or not training:
         return x
+
     keep_prob = 1 - drop_prob
-    shape = (x.shape[0],) + (1,) * (
-        x.ndim - 1
-    )  # work with diff dim tensors, not just 2D ConvNets
-    random_tensor = x.new_empty(shape).bernoulli_(keep_prob)
+    shape = (x.shape[0],) + (1,) * (x.ndim - 1)
+    random_tensor = mx.random.bernoulli(keep_prob, shape=shape).astype(x.dtype)
+
     if keep_prob > 0.0 and scale_by_keep:
-        random_tensor.div_(keep_prob)
+        random_tensor = random_tensor / keep_prob
+
     return x * random_tensor
 
 
 class DropPath(nn.Module):
-    """Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks)."""
-
     def __init__(self, drop_prob: float = 0.0, scale_by_keep: bool = True):
         super(DropPath, self).__init__()
         self.drop_prob = drop_prob
         self.scale_by_keep = scale_by_keep
 
-    def forward(self, x):
+    def __call__(self, x: mx.array) -> mx.array:
         return drop_path(x, self.drop_prob, self.training, self.scale_by_keep)
 
     def extra_repr(self):
---
+++ tests/test_drop.py
@@ -0,0 +1,71 @@
+import mlx.core as mx
+import numpy as np
+import pytest
+import timm.layers.drop as timm_m
+
+import mlx_im.layers.drop as mlx_m
+
+from . import utils as U
+
+
+@pytest.mark.parametrize("drop_prob", [0.0, 0.1])
+@pytest.mark.parametrize("batchwise", [True, False])
+@pytest.mark.parametrize("with_noise", [True, False])
+@pytest.mark.parametrize("fast", [True, False])
+@pytest.mark.parametrize("training", [True, False])
+def test_drop_block(drop_prob, batchwise, with_noise, fast, training):
+    x_mlx = U.sample_mlx_array_2d(shape=(1, 512, 768, 32))
+    x_torch = U.mlx_to_torch_2d(x_mlx)
+
+    mod_mlx = mlx_m.DropBlock2d(
+        drop_prob=drop_prob, batchwise=batchwise, with_noise=with_noise, fast=fast
+    )
+    mod_timm = timm_m.DropBlock2d(
+        drop_prob=drop_prob, batchwise=batchwise, with_noise=with_noise, fast=fast
+    )
+
+    mod_mlx = mod_mlx.train(training)
+    mod_timm = mod_timm.train(training)
+
+    out_mlx = mod_mlx(x_mlx)
+    out_timm = mod_timm(x_torch)
+
+    mx.eval(out_mlx)
+
+    out_mlx = U.mlx_to_numpy_2d(out_mlx)
+    out_timm = U.torch_to_numpy_2d(out_timm)
+
+    if drop_prob == 0.0 or not training:
+        assert np.allclose(
+            out_mlx, out_timm, atol=1.0e-7
+        ), f"{np.max(np.abs(out_mlx - out_timm)).item()}"
+    else:
+        assert out_mlx.shape == tuple(out_timm.shape)
+
+
+@pytest.mark.parametrize("drop_prob", [0.0, 0.1])
+@pytest.mark.parametrize("training", [True, False])
+def test_drop_path(drop_prob, training):
+    x_mlx = U.sample_mlx_array_2d(shape=(1, 512, 768, 32))
+    x_torch = U.mlx_to_torch_2d(x_mlx)
+
+    mod_mlx = mlx_m.DropPath(drop_prob=drop_prob)
+    mod_timm = timm_m.DropPath(drop_prob=drop_prob)
+
+    mod_mlx = mod_mlx.train(training)
+    mod_timm = mod_timm.train(training)
+
+    out_mlx = mod_mlx(x_mlx)
+    out_timm = mod_timm(x_torch)
+
+    mx.eval(out_mlx)
+
+    out_mlx = U.mlx_to_numpy_2d(out_mlx)
+    out_timm = U.torch_to_numpy_2d(out_timm)
+
+    if drop_prob == 0.0 or not training:
+        assert np.allclose(
+            out_mlx, out_timm, atol=1.0e-7
+        ), f"{np.max(np.abs(out_mlx - out_timm)).item()}"
+    else:
+        assert out_mlx.shape == tuple(out_timm.shape)
