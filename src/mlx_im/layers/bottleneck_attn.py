99b827b000287dbea5902eed9250e69d638c04ca
Louis Lac <lac.louis5@gmail.com>
2025-01-02 00:03:01 +0100

    [DEV] Bottleneck attn

--- src/mlx_im/layers/bottleneck_attn.py
+++ src/mlx_im/layers/bottleneck_attn.py
@@ -1,187 +1,133 @@
-"""Bottleneck Self Attention (Bottleneck Transformers)
-
-Paper: `Bottleneck Transformers for Visual Recognition` - https://arxiv.org/abs/2101.11605
-
-@misc{2101.11605,
-Author = {Aravind Srinivas and Tsung-Yi Lin and Niki Parmar and Jonathon Shlens and Pieter Abbeel and Ashish Vaswani},
-Title = {Bottleneck Transformers for Visual Recognition},
-Year = {2021},
-}
-
-Based on ref gist at: https://gist.github.com/aravindsrinivas/56359b79f0ce4449bcb04ab4b56a57a2
-
-This impl is a WIP but given that it is based on the ref gist likely not too far off.
-
-Hacked together by / Copyright 2021 Ross Wightman
-"""
-
 from typing import List
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+import mlx.core as mx
+from mlx import nn
 
-from .helpers import to_2tuple, make_divisible
-from .weight_init import trunc_normal_
+from .helpers import make_divisible, to_2tuple
 from .trace_utils import _assert
 
 
-def rel_logits_1d(q, rel_k, permute_mask: List[int]):
-    """Compute relative logits along one dimension
+def rel_logits_1d(q: mx.array, rel_k: mx.array, permute_mask: List[int]):
+    B, H, W, _ = q.shape
 
-    As per: https://gist.github.com/aravindsrinivas/56359b79f0ce4449bcb04ab4b56a57a2
-    Originally from: `Attention Augmented Convolutional Networks` - https://arxiv.org/abs/1904.09925
-
-    Args:
-        q: (batch, heads, height, width, dim)
-        rel_k: (2 * width - 1, dim)
-        permute_mask: permute output dim according to this
-    """
-    B, H, W, dim = q.shape
-    x = q @ rel_k.transpose(-1, -2)
+    x = q @ rel_k.transpose(1, 0)
     x = x.reshape(-1, W, 2 * W - 1)
-
-    # pad to shift from relative to absolute indexing
-    x_pad = F.pad(x, [0, 1]).flatten(1)
-    x_pad = F.pad(x_pad, [0, W - 1])
-
-    # reshape and slice out the padded elements
+    x_pad = mx.pad(x, pad_width=[(0, 0), (0, 0), (0, 1)]).flatten(1)
+    x_pad = mx.pad(x_pad, [(0, 0), (0, W - 1)])
     x_pad = x_pad.reshape(-1, W + 1, 2 * W - 1)
     x = x_pad[:, :W, W - 1 :]
 
-    # reshape and tile
-    x = x.reshape(B, H, 1, W, W).expand(-1, -1, H, -1, -1)
-    return x.permute(permute_mask)
+    x = x.reshape(B, H, 1, W, W)
+    x = mx.broadcast_to(x, (B, H, H, W, W))
+
+    return x.transpose(permute_mask)
 
 
 class PosEmbedRel(nn.Module):
-    """Relative Position Embedding
-    As per: https://gist.github.com/aravindsrinivas/56359b79f0ce4449bcb04ab4b56a57a2
-    Originally from: `Attention Augmented Convolutional Networks` - https://arxiv.org/abs/1904.09925
-    """
-
-    def __init__(self, feat_size, dim_head, scale):
+    def __init__(self, feat_size: int | tuple[int, int], dim_head: int, scale: float):
         super().__init__()
         self.height, self.width = to_2tuple(feat_size)
         self.dim_head = dim_head
-        self.height_rel = nn.Parameter(
-            torch.randn(self.height * 2 - 1, dim_head) * scale
-        )
-        self.width_rel = nn.Parameter(torch.randn(self.width * 2 - 1, dim_head) * scale)
 
-    def forward(self, q):
+        self.height_rel = mx.random.normal((self.height * 2 - 1, dim_head)) * scale
+        self.width_rel = mx.random.normal((self.width * 2 - 1, dim_head)) * scale
+
+    # (B, H*W, C)
+    def __call__(self, q: mx.array) -> mx.array:
         B, HW, _ = q.shape
 
-        # relative logits in width dimension.
+        # (B, H, W, C) -> (B, H, W, H, W)
         q = q.reshape(B, self.height, self.width, -1)
         rel_logits_w = rel_logits_1d(q, self.width_rel, permute_mask=(0, 1, 3, 2, 4))
 
-        # relative logits in height dimension.
-        q = q.transpose(1, 2)
+        # (B, W, H, C) -> (B, H, W, H, W)
+        q = q.transpose(0, 2, 1, 3)
         rel_logits_h = rel_logits_1d(q, self.height_rel, permute_mask=(0, 3, 1, 4, 2))
 
+        # (B, H, W, H, W) -> (B, HW, HW)
         rel_logits = rel_logits_h + rel_logits_w
         rel_logits = rel_logits.reshape(B, HW, HW)
         return rel_logits
 
 
 class BottleneckAttn(nn.Module):
-    """Bottleneck Attention
-    Paper: `Bottleneck Transformers for Visual Recognition` - https://arxiv.org/abs/2101.11605
-
-    The internal dimensions of the attention module are controlled by the interaction of several arguments.
-      * the output dimension of the module is specified by dim_out, which falls back to input dim if not set
-      * the value (v) dimension is set to dim_out // num_heads, the v projection determines the output dim
-      * the query and key (qk) dimensions are determined by
-        * num_heads * dim_head if dim_head is not None
-        * num_heads * (dim_out * attn_ratio // num_heads) if dim_head is None
-      * as seen above, attn_ratio determines the ratio of q and k relative to the output if dim_head not used
-
-    Args:
-        dim (int): input dimension to the module
-        dim_out (int): output dimension of the module, same as dim if not set
-        stride (int): output stride of the module, avg pool used if stride == 2 (default: 1).
-        num_heads (int): parallel attention heads (default: 4)
-        dim_head (int): dimension of query and key heads, calculated from dim_out * attn_ratio // num_heads if not set
-        qk_ratio (float): ratio of q and k dimensions to output dimension when dim_head not set. (default: 1.0)
-        qkv_bias (bool): add bias to q, k, and v projections
-        scale_pos_embed (bool): scale the position embedding as well as Q @ K
-    """
-
     def __init__(
         self,
-        dim,
-        dim_out=None,
-        feat_size=None,
-        stride=1,
-        num_heads=4,
-        dim_head=None,
-        qk_ratio=1.0,
-        qkv_bias=False,
-        scale_pos_embed=False,
+        dim: int,
+        dim_out: int | None = None,
+        feat_size: int | tuple[int, int] | None = None,
+        stride: int = 1,
+        num_heads: int = 4,
+        dim_head: int | None = None,
+        qk_ratio: float = 1.0,
+        qkv_bias: bool = False,
+        scale_pos_embed: bool = False,
     ):
         super().__init__()
         assert (
             feat_size is not None
         ), "A concrete feature size matching expected input (H, W) is required"
-        dim_out = dim_out or dim
+
+        dim_out = dim_out or dim  # O
         assert dim_out % num_heads == 0
-        self.num_heads = num_heads
+
+        self.num_heads = num_heads  # N
         self.dim_head_qk = (
             dim_head or make_divisible(dim_out * qk_ratio, divisor=8) // num_heads
-        )
-        self.dim_head_v = dim_out // self.num_heads
-        self.dim_out_qk = num_heads * self.dim_head_qk
-        self.dim_out_v = num_heads * self.dim_head_v
+        )  # O1/N
+        self.dim_head_v = dim_out // self.num_heads  # O2/N
+        self.dim_out_qk = num_heads * self.dim_head_qk  # O1
+        self.dim_out_v = num_heads * self.dim_head_v  # O2
         self.scale = self.dim_head_qk**-0.5
         self.scale_pos_embed = scale_pos_embed
 
+        # D -> 2*O1 + O2
         self.qkv = nn.Conv2d(
             dim, self.dim_out_qk * 2 + self.dim_out_v, 1, bias=qkv_bias
         )
 
-        # NOTE I'm only supporting relative pos embedding for now
         self.pos_embed = PosEmbedRel(
             feat_size, dim_head=self.dim_head_qk, scale=self.scale
         )
 
         self.pool = nn.AvgPool2d(2, 2) if stride == 2 else nn.Identity()
 
-        self.reset_parameters()
+    def __call__(self, x: mx.array) -> mx.array:
+        B, H, W, _ = x.shape
 
-    def reset_parameters(self):
-        trunc_normal_(self.qkv.weight, std=self.qkv.weight.shape[1] ** -0.5)  # fan-in
-        trunc_normal_(self.pos_embed.height_rel, std=self.scale)
-        trunc_normal_(self.pos_embed.width_rel, std=self.scale)
-
-    def forward(self, x):
-        B, C, H, W = x.shape
         _assert(H == self.pos_embed.height, "")
         _assert(W == self.pos_embed.width, "")
 
-        x = self.qkv(x)  # B, (2 * dim_head_qk + dim_head_v) * num_heads, H, W
+        # (B, H, W, 2*O1+O2)
+        x = self.qkv(x)
 
-        # NOTE head vs channel split ordering in qkv projection was decided before I allowed qk to differ from v
-        # So, this is more verbose than if heads were before qkv splits, but throughput is not impacted.
-        q, k, v = torch.split(
-            x, [self.dim_out_qk, self.dim_out_qk, self.dim_out_v], dim=1
-        )
-        q = q.reshape(B * self.num_heads, self.dim_head_qk, -1).transpose(-1, -2)
-        k = k.reshape(
-            B * self.num_heads, self.dim_head_qk, -1
-        )  # no transpose, for q @ k
-        v = v.reshape(B * self.num_heads, self.dim_head_v, -1).transpose(-1, -2)
+        q, k, v = mx.split(x, [self.dim_out_qk, 2 * self.dim_out_qk], axis=-1)
+
+        # (B, H, W, N*O1) -> (B, H*W, N, O1/N) -> (B, N, H*W, O1/N)
+        q = q.reshape(B, -1, self.num_heads, self.dim_head_qk).transpose(0, 2, 1, 3)
+
+        # (B, H, W, N*O1) -> (B, H*W, N, O1/N) -> (B, N, H*W, O1/N)
+        k = k.reshape(B, -1, self.num_heads, self.dim_head_qk).transpose(0, 2, 1, 3)
+
+        # (B, H, W, N*O2) -> (B, H*W, N, O2/N) -> (B, N, H*W, O2/N)
+        v = v.reshape(B, -1, self.num_heads, self.dim_head_v).transpose(0, 2, 1, 3)
+
+        # (B*N, H*W, 01/N)
+        pe = self.pos_embed(q.reshape(B * self.num_heads, H * W, -1))
 
         if self.scale_pos_embed:
-            attn = (
-                q @ k + self.pos_embed(q)
-            ) * self.scale  # B * num_heads, H * W, H * W
+            # (B, N, H*W, O2/N)
+            out = mx.fast.scaled_dot_product_attention(
+                q, k, v, scale=self.scale, mask=pe * self.scale
+            )
         else:
-            attn = (q @ k) * self.scale + self.pos_embed(q)
-        attn = attn.softmax(dim=-1)
+            # (B, N, H*W, O2/N)
+            out = mx.fast.scaled_dot_product_attention(
+                q, k, v, scale=self.scale, mask=pe
+            )
 
-        out = (
-            (attn @ v).transpose(-1, -2).reshape(B, self.dim_out_v, H, W)
-        )  # B, dim_out, H, W
-        out = self.pool(out)
-        return out
+        # (B, N, H*W, O2/N)
+        out = out.transpose(0, 2, 1, 3).reshape(B, H, W, -1)
+
+        # (B, H/2, W/2, O2)
+        return self.pool(out)
---
+++ tests/test_bottleneck_attn.py
@@ -0,0 +1,109 @@
+import mlx.core as mx
+import numpy as np
+import pytest
+import timm.layers.bottleneck_attn as timm_m
+import torch
+
+import mlx_im.layers.bottleneck_attn as mlx_m
+
+from . import utils as U
+
+
+def test_rel_logits_1d():
+    x_mlx = mx.random.normal(shape=(1, 32, 48, 128))
+    x_torch = torch.from_numpy(np.array(x_mlx))
+
+    rel_k_mlx = mx.random.normal(shape=(2 * 48 - 1, 128))
+    rel_k_torch = torch.from_numpy(np.array(rel_k_mlx))
+
+    mod_mlx = mlx_m.rel_logits_1d
+    mod_timm = timm_m.rel_logits_1d
+
+    out_timm = mod_timm(q=x_torch, rel_k=rel_k_torch, permute_mask=(0, 1, 3, 2, 4))
+    out_mlx = mod_mlx(q=x_mlx, rel_k=rel_k_mlx, permute_mask=(0, 1, 3, 2, 4))
+    mx.eval(out_mlx)
+
+    out_mlx = np.array(out_mlx)
+    out_timm = out_timm.detach().numpy()
+
+    assert np.allclose(
+        out_mlx, out_timm, atol=1.0e-7
+    ), f"{np.max(np.abs(out_mlx - out_timm)).item()}"
+
+
+@pytest.mark.parametrize("scale", [1.0, 0.5, 1.5])
+def test_pos_emb_rel(scale):
+    torch.manual_seed(42)
+
+    x_mlx = mx.random.normal(shape=(1, 32 * 48, 128))
+    x_torch = torch.from_numpy(np.array(x_mlx))
+
+    mod_mlx = mlx_m.PosEmbedRel(feat_size=(32, 48), dim_head=128, scale=scale)
+    mod_timm = timm_m.PosEmbedRel(feat_size=(32, 48), dim_head=128, scale=scale)
+
+    mod_mlx.height_rel = mx.array(mod_timm.height_rel.detach().numpy())
+    mod_mlx.width_rel = mx.array(mod_timm.width_rel.detach().numpy())
+
+    out_mlx = mod_mlx(q=x_mlx)
+    out_timm = mod_timm(q=x_torch)
+
+    mx.eval(out_mlx)
+
+    out_mlx = np.array(out_mlx)
+    out_timm = out_timm.detach().numpy()
+
+    assert np.allclose(
+        out_mlx, out_timm, atol=1.0e-9
+    ), f"{np.max(np.abs(out_mlx - out_timm)).item()}"
+
+
+@pytest.mark.parametrize("dim_out", [128, 256])
+@pytest.mark.parametrize("bias", [True, False])
+@pytest.mark.parametrize("scale_pos_emb", [True, False])
+def test_bottleneck_attn(dim_out, bias, scale_pos_emb):
+    torch.manual_seed(42)
+
+    x_mlx = U.sample_mlx_array_2d(shape=(1, 32, 48, 128))
+    x_torch = U.mlx_to_torch_2d(x_mlx)
+
+    mod_mlx = mlx_m.BottleneckAttn(
+        dim=128,
+        dim_out=dim_out,
+        feat_size=(32, 48),
+        dim_head=64,
+        qkv_bias=bias,
+        scale_pos_embed=scale_pos_emb,
+    )
+    mod_timm = timm_m.BottleneckAttn(
+        dim=128,
+        dim_out=dim_out,
+        feat_size=(32, 48),
+        dim_head=64,
+        qkv_bias=bias,
+        scale_pos_embed=scale_pos_emb,
+    )
+
+    mod_mlx.pos_embed.height_rel = mx.array(
+        mod_timm.pos_embed.height_rel.detach().numpy()
+    )
+    mod_mlx.pos_embed.width_rel = mx.array(
+        mod_timm.pos_embed.width_rel.detach().numpy()
+    )
+
+    mod_mlx.qkv.weight = mx.array(mod_timm.qkv.weight.detach().numpy()).transpose(
+        0, 2, 3, 1
+    )
+
+    if bias:
+        mod_mlx.qkv.bias = mx.array(mod_timm.qkv.bias.detach().numpy())
+
+    out_timm = mod_timm(x_torch)
+    out_mlx = mod_mlx(x_mlx)
+    mx.eval(out_mlx)
+
+    out_mlx = U.mlx_to_numpy_2d(out_mlx)
+    out_timm = U.torch_to_numpy_2d(out_timm)
+
+    assert np.allclose(
+        out_mlx, out_timm, atol=1.0e-5
+    ), f"{np.max(np.abs(out_mlx - out_timm)).item()}"
