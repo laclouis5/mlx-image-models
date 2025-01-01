ab9e60a1db496fe97825c049ba47b06c820af5b4
Louis Lac <lac.louis5@gmail.com>
2025-01-01 17:17:05 +0100

    [DEV] MQA 2d

--- src/mlx_im/layers/attention2d.py
+++ src/mlx_im/layers/attention2d.py
@@ -77,13 +77,13 @@ class MultiQueryAttentionV2(nn.Module):
 class MultiQueryAttention2d(nn.Module):
     def __init__(
         self,
-        dim: int,
-        dim_out: Optional[int] = None,
-        num_heads: int = 8,
-        key_dim: Optional[int] = None,
-        value_dim: Optional[int] = None,
-        query_strides: int = 1,
-        kv_stride: int = 1,
+        dim: int,  # D
+        dim_out: Optional[int] = None,  # O
+        num_heads: int = 8,  # N
+        key_dim: Optional[int] = None,  # K
+        value_dim: Optional[int] = None,  # V
+        query_strides: int = 1,  # sq
+        kv_stride: int = 1,  # skv
         dw_kernel_size: int = 3,
         dilation: int = 1,
         padding: Union[str, int, List[int]] = "",
@@ -97,8 +97,8 @@ class MultiQueryAttention2d(nn.Module):
         self.num_heads = num_heads  # N
         self.key_dim = key_dim or dim // num_heads  # K
         self.value_dim = value_dim or dim // num_heads  # V
-        self.query_strides = to_2tuple(query_strides)
-        self.kv_stride = kv_stride
+        self.query_strides = to_2tuple(query_strides)  # sq
+        self.kv_stride = kv_stride  # skv
         self.has_query_strides = any([s > 1 for s in self.query_strides])
         self.scale = self.key_dim**-0.5
         self.fused_attn = use_fused_attn()
@@ -109,9 +109,7 @@ class MultiQueryAttention2d(nn.Module):
             if padding == "same":
                 query.append(
                     create_pool2d(
-                        "avg",
-                        kernel_size=self.query_strides,
-                        padding="same",
+                        "avg", kernel_size=self.query_strides, padding="same"
                     ),
                 )
             else:
@@ -119,13 +117,10 @@ class MultiQueryAttention2d(nn.Module):
             query.append(norm_layer(dim))
         query.append(
             create_conv2d(
-                dim,
-                self.num_heads * self.key_dim,
-                kernel_size=1,
-                bias=use_bias,
+                dim, self.num_heads * self.key_dim, kernel_size=1, bias=use_bias
             ),
         )
-        self.query = nn.Sequential(query)
+        self.query = nn.Sequential(*query)
 
         key = []
         if kv_stride > 1:
@@ -143,14 +138,10 @@ class MultiQueryAttention2d(nn.Module):
             key.append(norm_layer(dim))
         key.append(
             create_conv2d(
-                dim,
-                self.key_dim,
-                kernel_size=1,
-                padding=padding,
-                bias=use_bias,
+                dim, self.key_dim, kernel_size=1, padding=padding, bias=use_bias
             ),
         )
-        self.key = nn.Sequential(key)
+        self.key = nn.Sequential(*key)
 
         value = []
         if kv_stride > 1:
@@ -167,14 +158,9 @@ class MultiQueryAttention2d(nn.Module):
             )
             value.append(norm_layer(dim))
         value.append(
-            create_conv2d(
-                dim,
-                self.value_dim,
-                kernel_size=1,
-                bias=use_bias,
-            ),
+            create_conv2d(dim, self.value_dim, kernel_size=1, bias=use_bias),
         )
-        self.value = nn.Sequential(value)
+        self.value = nn.Sequential(*value)
 
         self.attn_drop = nn.Dropout(attn_drop)
 
@@ -183,98 +169,59 @@ class MultiQueryAttention2d(nn.Module):
             output.append(
                 nn.Upsample(
                     scale_factor=self.query_strides,
-                    mode="bilinear",
+                    mode="linear",
                     align_corners=False,
                 ),
             )
         output.append(
             create_conv2d(
-                self.value_dim * self.num_heads,
-                dim_out,
-                kernel_size=1,
-                bias=use_bias,
+                self.value_dim * self.num_heads, dim_out, kernel_size=1, bias=use_bias
             ),
         )
         output.append(nn.Dropout(proj_drop))
-        self.output = nn.Sequential(output)
-
-        self.einsum = False
+        self.output = nn.Sequential(*output)
 
     def _reshape_input(self, t: mx.array) -> mx.array:
-        s = t.shape
-        t = t.reshape(s[0], -1, s[1])  # (B, H*W, C)
-        if self.einsum:
-            return t
-        else:
-            return t[:, None, ...]  # (B, 1, H*W, C)
+        b, _, _, k = t.shape  # (B, H, W, K)
+        t = t.reshape(b, -1, k)  # (B, H*W, K)
+        return t[:, None, ...]  # (B, 1, H*W, K)
 
     def _reshape_projected_query(self, t: mx.array, num_heads: int, key_dim: int):
-        """Reshapes projected query: [b, n, n, h x k] -> [b, n x n, h, k]."""
-        s = t.shape
-        t = t.reshape(s[0], num_heads, key_dim, -1)
-        if self.einsum:
-            return t.permute(0, 3, 1, 2).contiguous()
-        else:
-            return t.transpose(-1, -2).contiguous()
+        b, _, _, _ = t.shape  # (B, H/sq, W/sq, N*K)
+        t = t.reshape(b, -1, num_heads, key_dim)  # (B, H*W/sq/sq, N, K)
+        return t.transpose(0, 2, 1, 3)  # (B, N, H*W/sq/sq, K)
 
     def _reshape_output(self, t: mx.array, num_heads: int, h_px: int, w_px: int):
-        """Reshape output:[b, n x n x h, k] -> [b, n, n, hk]."""
-        s = t.shape
-        feat_dim = s[-1] * num_heads
-        if not self.einsum:
-            t = t.transpose(1, 2)
-        return t.reshape(s[0], h_px, w_px, feat_dim).permute(0, 3, 1, 2).contiguous()
+        b, _, _, _ = t.shape  # (B, N, H*W/sq/sq, V)
+        t = t.transpose(0, 2, 1, 3)  # (B, H*W/sq/sq, N, V)
+        return t.reshape(b, h_px, w_px, -1)  # (B, H/sq, *W/sq, N*V)
 
     def __call__(self, x: mx.array, attn_mask: Optional[mx.array] = None) -> mx.array:
-        """Run layer computation."""
-        _, _, H, W = x.shape
+        _, h, w, _ = x.shape
 
+        # (B, H, W, D) -> (B, H/sq, W/sq, N*K) -> (B, N, H*W/sq/sq, K)
         q = self.query(x)
-        # desired q shape: [b, h, k, n x n] - [b, l, h, k]
         q = self._reshape_projected_query(q, self.num_heads, self.key_dim)
 
+        # (B, H, W, D) -> (B, H/skv, W/skv, K) -> (B, 1, H*W/skv/skv, K)
         k = self.key(x)
-        # output shape of k: [b, k, p], p = m x m
         k = self._reshape_input(k)
 
+        # (B, H, W, D) -> (B, H/skv, W/skv, V) -> (B, 1, H*W/skv/skv, V)
         v = self.value(x)
-        # output shape of v: [ b, p, k], p = m x m
         v = self._reshape_input(v)
 
-        # desired q shape: [b, n x n, h, k]
-        # desired k shape: [b, m x m, k]
-        # desired logits shape: [b, n x n, h, m x m]
-        if self.einsum:
-            attn = mx.einsum("blhk,bpk->blhp", q, k) * self.scale
-            if attn_mask is not None:
-                # NOTE: assumes mask is float and in correct shape
-                attn = attn + attn_mask
-            attn = attn.softmax(dim=-1)
-            attn = self.attn_drop(attn)
-            o = mx.einsum("blhp,bpk->blhk", attn, v)
-        else:
-            if self.fused_attn:
-                o = scaled_dot_product_attention(
-                    q,
-                    k,
-                    v,
-                    attn_mask=attn_mask,
-                    dropout_p=self.attn_drop.p if self.training else 0.0,
-                )
-            else:
-                q = q * self.scale
-                attn = q @ k.transpose(-1, -2)
-                if attn_mask is not None:
-                    # NOTE: assumes mask is float and in correct shape
-                    attn = attn + attn_mask
-                attn = attn.softmax(dim=-1)
-                attn = self.attn_drop(attn)
-                o = attn @ v
-
-        # reshape o into [b, hk, n, n,]
-        o = self._reshape_output(
-            o, self.num_heads, H // self.query_strides[0], W // self.query_strides[1]
+        # (B, N, H*W/sq/sq, V)
+        o = mx.fast.scaled_dot_product_attention(
+            q, k, v, scale=self.scale, mask=attn_mask
         )
+
+        # (B, N, H*W/sq/sq, V) -> (B, H/sq, W/sq, N*V)
+        o = self._reshape_output(
+            o, self.num_heads, h // self.query_strides[0], w // self.query_strides[1]
+        )
+
+        # (B, H/sq, W/sq, N*V) -> (B, H, W, O)
         x = self.output(o)
         return x
 
--- tests/test_attention2d.py
+++ tests/test_attention2d.py
@@ -116,3 +116,104 @@ def test_mqa_v2(dim_out, attn_mask):
     assert np.allclose(
         out_mlx, out_timm, atol=1.0e-4
     ), f"{np.max(np.abs(out_mlx - out_timm)).item()}"
+
+
+@pytest.mark.parametrize("dim", [128])
+@pytest.mark.parametrize("dim_out", [128, 256])
+@pytest.mark.parametrize("query_strides", [1, 2])
+@pytest.mark.parametrize("kv_stride", [1, 2])
+@pytest.mark.parametrize("use_bias", [True, False])
+@pytest.mark.parametrize("attn_mask", [True, False])
+def test_mqa(dim, dim_out, query_strides, kv_stride, use_bias, attn_mask):
+    torch.manual_seed(42)
+
+    x_mlx = U.sample_mlx_array_2d(shape=(1, 32, 48, dim))
+    x_torch = U.mlx_to_torch_2d(x_mlx)
+
+    mod_mlx = mlx_m.MultiQueryAttention2d(
+        dim=dim,
+        dim_out=dim_out,
+        key_dim=64,
+        value_dim=64,
+        query_strides=query_strides,
+        kv_stride=kv_stride,
+        padding="same",
+        use_bias=use_bias,
+    )
+    mod_timm = timm_m.MultiQueryAttention2d(
+        dim=dim,
+        dim_out=dim_out,
+        key_dim=64,
+        value_dim=64,
+        query_strides=query_strides,
+        kv_stride=kv_stride,
+        padding="same",
+        use_bias=use_bias,
+    )
+
+    mod_mlx.query.layers[-1].weight = mx.array(
+        mod_timm.query[-1].weight.detach().numpy()
+    ).transpose(0, 2, 3, 1)
+
+    mod_mlx.key.layers[-1].weight = mx.array(
+        mod_timm.key[-1].weight.detach().numpy()
+    ).transpose(0, 2, 3, 1)
+
+    mod_mlx.value.layers[-1].weight = mx.array(
+        mod_timm.value[-1].weight.detach().numpy()
+    ).transpose(0, 2, 3, 1)
+
+    mod_mlx.output.layers[-2].weight = mx.array(
+        mod_timm.output[-2].weight.detach().numpy()
+    ).transpose(0, 2, 3, 1)
+
+    if use_bias:
+        mod_mlx.query.layers[-1].bias = mx.array(
+            mod_timm.query[-1].bias.detach().numpy()
+        )
+
+        mod_mlx.key.layers[-1].bias = mx.array(
+            mod_timm.key[-1].bias.detach().numpy(),
+        )
+
+        mod_mlx.value.layers[-1].bias = mx.array(
+            mod_timm.value[-1].bias.detach().numpy()
+        )
+
+        mod_mlx.output.layers[-2].bias = mx.array(
+            mod_timm.output[-2].bias.detach().numpy()
+        )
+
+    if kv_stride > 1:
+        mod_mlx.key.layers[0].weight = mx.array(
+            mod_timm.key[0].weight.detach().numpy()
+        ).transpose(0, 2, 3, 1)
+
+        mod_mlx.value.layers[0].weight = mx.array(
+            mod_timm.value[0].weight.detach().numpy()
+        ).transpose(0, 2, 3, 1)
+
+    if attn_mask:
+        mask_mlx = (
+            -mx.random.randint(
+                low=0,
+                high=1,
+                shape=(32 * 48 // query_strides**2, 32 * 48 // kv_stride**2),
+                key=mx.array([42, 42], dtype=mx.uint32),
+            )
+            * 1e7
+        )
+        mask_torch = torch.from_numpy(np.array(mask_mlx))
+    else:
+        mask_mlx = mask_torch = None
+
+    out_mlx = mod_mlx(x_mlx, attn_mask=mask_mlx)
+    out_timm = mod_timm(x_torch, attn_mask=mask_torch)
+    mx.eval(out_mlx)
+
+    out_mlx = U.mlx_to_numpy_2d(out_mlx)
+    out_timm = U.torch_to_numpy_2d(out_timm)
+
+    assert np.allclose(
+        out_mlx, out_timm, atol=1.0e-6
+    ), f"{np.max(np.abs(out_mlx - out_timm)).item()}"
